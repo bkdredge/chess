87de9258572a6352f3cb1615400d3b4d80e8f94f
Braden Dredge <braden.dredge@gmail.com>
2023-10-17 08:46:55 -0600

    PHASE 2: add(Javadoc) for tests, add some unit test FIXMEs

---
+++ src/starter/unitTests/RequestTests.java
@@ -0,0 +1,11 @@
+package unitTests;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for requests
+ */
+public class RequestTests {
+}
