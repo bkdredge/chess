008bc4908401c9d23fc109951e7e826bef51e807
Braden Dredge <braden.dredge@gmail.com>
2023-10-25 21:13:23 -0600

    PHASE 3: Configured project for phase 3, all downloads

---
+++ src/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"chess"},{"l":"dataAccess"},{"l":"handler"},{"l":"model"},{"l":"request"},{"l":"result"},{"l":"service"},{"l":"unitTests"}];updateSearchResults();
